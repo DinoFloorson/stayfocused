422b4268c095bdba2265e86ac61bcba5ba4d3d55
JuleFloorson <jubo93@web.de>
2020-04-07 15:26:46 +0200

    Configuration from prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es6: true,
     node: true,
+    jest: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'prettier', 'plugin:react/recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
