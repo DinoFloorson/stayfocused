7bcd770b0c2d40950f0eb859cff5d8955a24af21
JuleFloorson <jubo93@web.de>
2020-04-13 15:53:46 +0200

    Add Day Button and Day Form

--- client/src/components/CalenderButton.js
+++ client/src/components/CalenderButton.js
@@ -4,6 +4,8 @@ const CalenderButton = styled.button`
   background-color: #011e62;
   border: none;
   height: 20px;
+  margin-left: 5px;
+  margin-right: 5px;
 `;
 
 export default CalenderButton;
--- client/src/stories/Calender.stories.js
+++ client/src/stories/Calender.stories.js
@@ -3,7 +3,7 @@ import CalenderForm from '../components/CalenderForm';
 import CalenderButton from '../components/CalenderButton';
 import ArrowLeft from '../assets/arrowleft.svg';
 import ArrowRight from '../assets/arrowright.svg';
-import DayArea from '../components/DayArea';
+import DayButton from '../components/DayButton';
 import DayText from '../components/DayText';
 
 export default {
@@ -16,27 +16,9 @@ export const Calender = () => (
       <CalenderButton>
         <img src={ArrowLeft} />
       </CalenderButton>
-      <DayArea>
+      <DayButton>
         <DayText>Mon</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Tue</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Wed</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Thur</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Fri</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Sat</DayText>
-      </DayArea>
-      <DayArea>
-        <DayText>Son</DayText>
-      </DayArea>
+      </DayButton>
 
       <CalenderButton>
         <img src={ArrowRight} />
