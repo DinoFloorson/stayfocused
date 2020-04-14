1e4040071b11e847a99f45a5b1323ea8caec6c11
JuleFloorson <jubo93@web.de>
2020-04-14 14:02:16 +0200

    Add Route as comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import './App.css';
+// import GlobalStyles from 'Globalstyles.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <GlobalStyles>
+//       <Router>
+//         <Route path="/" />
+//         <Route path="/home" />
+//       </Router>
+//     </GlobalStyles>
+//   );
+// }
 
-export default App;
+// export default App;
