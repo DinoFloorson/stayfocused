190cfd8c0afb7cfdb33908459c61b7672b467fa9
JuleFloorson <jubo93@web.de>
2020-04-20 10:15:03 +0200

    Add Button to add new Task

---
+++ client/src/stories/AddButton.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddButton from '../components/AddButton';
+
+export default {
+  title: 'Button',
+};
+
+export const NewAddButton = () => (
+  <>
+    <AddButton>+</AddButton>
+  </>
+);
