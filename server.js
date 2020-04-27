9ae3d835811370f2c8eb0ececa1ea45c76feeb8e
JuleFloorson <jubo93@web.de>
2020-04-27 17:39:32 +0200

    Delete comments

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-// app.get('*', (request, response) => {
-//   response.sendFile(path.join(__dirname, 'client/build/index.html'));
-// });
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is running http://localhost:${port}`);
-// });
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
