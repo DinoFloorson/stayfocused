093be651a15fdd82cafdcd2d8da8fdcf73829b10
JuleFloorson <jubo93@web.de>
2020-04-28 09:38:41 +0200

    Fix bug in json server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'client/build'),
+});
 
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-);
 server.use(middlewares);
-server.use(router);
+server.use('/api', router);
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 server.listen(8080, () => {
   console.log('JSON Server is running');
 });
